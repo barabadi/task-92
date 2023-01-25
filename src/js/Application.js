d757b099c497dcf9ae368b21ad7f3b5fbd3ad429
barabadi <101886037+barabadi@users.noreply.github.com>
2023-01-25 20:03:42 +0200

    Update Application.js

--- src/js/Application.js
+++ src/js/Application.js
@@ -1,4 +1,6 @@
-import EventEmitter from "eventemitter3";
+iimport EventEmitter from "eventemitter3";
+import Card from "./Card";
+import Notification from "./Notification";
 
 export default class Application extends EventEmitter {
   static get events() {
@@ -10,9 +12,29 @@ export default class Application extends EventEmitter {
   constructor() {
     super();
 
-    const button = document.querySelector(".button");
-    button.addEventListener("click", () => {
-      alert("💣");
+    let pizzas = [
+      {
+        type: "Hawaiian",
+        emoji: "🍍",
+        price: 13.99,
+      },
+      {
+        type: "Pepperoni",
+        emoji: "🍕",
+        price: 14.99,
+      },
+      {
+        type: "Margherita",
+        emoji: "🍅",
+        price: 11.99,
+      },
+    ];
+
+    pizzas.forEach((pizza) => {
+      const card = new Card({ ...pizza });
+      card.render();
+      card.on(Card.events.ADD_TO_CART, (obj) => new Notification().render(obj));
+      document.querySelector(".main").appendChild(card.container);
     });
 
     this.emit(Application.events.READY);
